489447af84c840bf2ed15c2dca12acd31835e9d3
adelinelucas <82570582+adelinelucas@users.noreply.github.com>
2022-07-31 23:03:18 +0200

    creation du fichier server.js + connection à la bdd

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const app = express();
+require('dotenv').config({path:'./config/.env'});
 const port = 5000;
+require('./config/db');
+
+// partie qui est lue en dernier
+app.listen(process.env.PORT, ()=>{
+    console.log(`listening on ${process.env.PORT} `);
+})
